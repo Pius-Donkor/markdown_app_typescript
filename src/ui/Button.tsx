5cad4309722b4aba5f0f56bf1ec2da0e5ad23186
Pius <piusdonkor1@gmail.com>
2024-07-05 06:01:48 +0000

    Create Button.tsx

---
+++ src/ui/Button.tsx
@@ -0,0 +1,33 @@
+import styled, { css } from "styled-components";
+
+interface ButtonProps {
+  large?: boolean;
+}
+
+const largeButton = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Button = styled.button.withConfig({
+  shouldForwardProp: (prop) => prop !== "large",
+})<ButtonProps>`
+  background-color: var(--color-orange-0);
+  padding: ${({ large }) => (large ? "10px 20px" : "10px")};
+  transition: background-color 0.3s ease;
+  font-size: var(--font-size-medium);
+  color: var(--color-white-0);
+  text-transform: capitalize;
+  border-radius: 3px;
+  display: flex;
+  align-items: center;
+  gap: 0.3rem;
+  &:hover {
+    background-color: var(--color-orange-100);
+  }
+  ${({ large }) => (large ? largeButton : "")}
+`;
+
+export default Button;
