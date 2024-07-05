d1bf80b7beb2c51f90234f7685a783b5df6282c2
Pius <piusdonkor1@gmail.com>
2024-07-05 06:02:07 +0000

    Delete Icon.jsx

--- src/ui/Icon.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const Icon = styled.img`
-  width: ${({ type }) => (type === "small" ? "15px" : "20px")};
-  height: ${({ type }) => (type === "small" ? "15px" : "20px")};
-  cursor: pointer;
-`;
-
-export default Icon;
