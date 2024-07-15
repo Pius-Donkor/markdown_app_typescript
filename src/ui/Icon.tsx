565da2f7cf6d11dc8f40e681ab6bee5cf8032d70
Pius <piusdonkor1@gmail.com>
2024-07-15 01:45:13 +0000

    Update Icon.tsx

--- src/ui/Icon.tsx
+++ src/ui/Icon.tsx
@@ -2,11 +2,20 @@ import styled from "styled-components";
 
 interface IconProps {
   type?: string | null;
+  hoverBackground?: string | null;
 }
+
+// const hoverStyle = css`
+//   fill: var(--color-orange-0);
+//   color: var(--color-orange-0);
+// `;
 
 const Icon = styled.img<IconProps>`
   width: ${({ type }) => (type === "small" ? "15px" : "20px")};
   height: ${({ type }) => (type === "small" ? "15px" : "20px")};
+
+  /* transition: fill color 300ms; */
+
   cursor: pointer;
 `;
 
