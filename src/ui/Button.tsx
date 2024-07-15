9a3c4f36cb1e07593161ae5532da27e94e4cd4bc
Pius <piusdonkor1@gmail.com>
2024-07-15 01:45:02 +0000

    Update Button.tsx

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -2,6 +2,7 @@ import styled, { css } from "styled-components";
 
 interface ButtonProps {
   large?: boolean;
+  $ismobile?: boolean;
 }
 
 const largeButton = css`
@@ -20,14 +21,20 @@ const Button = styled.button.withConfig({
   font-size: var(--font-size-medium);
   color: var(--color-white-0);
   text-transform: capitalize;
+
   border-radius: 3px;
   display: flex;
+  justify-content: center;
   align-items: center;
   gap: 0.3rem;
   &:hover {
     background-color: var(--color-orange-100);
   }
   ${({ large }) => (large ? largeButton : "")}
+
+  @media screen and (max-width: 700px) {
+    padding: ${({ $ismobile }) => ($ismobile ? "10px" : "10px 20px")};
+  }
 `;
 
 export default Button;
