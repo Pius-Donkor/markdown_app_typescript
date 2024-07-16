de4f30522185f26a6222dd0668cac190ceea5633
Pius <piusdonkor1@gmail.com>
2024-07-16 05:46:04 +0000

    Update ContentBar.tsx

--- src/ui/ContentBar.tsx
+++ src/ui/ContentBar.tsx
@@ -1,14 +1,16 @@
 import styled from "styled-components";
 
-const ContentBar = styled.div`
+const ContentBar = styled.div<{ $isDark?: boolean }>`
   width: 100%;
   height: 3rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 0 20px;
-  background-color: var(--color-white-100);
-  color: var(--color-grey-100);
+  background-color: ${({ $isDark }) =>
+    $isDark ? "var(--color-dark-100)" : "var(--color-white-100)"};
+  color: ${({ $isDark }) =>
+    $isDark ? "var(--color-grey-200)" : "var(--color-grey-100)"};
   font-size: var(--font-size-medium);
   text-transform: uppercase;
   letter-spacing: var(--char-spacing);
